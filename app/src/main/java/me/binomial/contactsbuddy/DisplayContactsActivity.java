01fc8c795695ec3be62d2c8d471d854eeb6f20c9
Amit Upadhyay <amit.upadhyay@cleartax.in>
2018-09-16 18:35:20 +0530

    update in Activity

--- app/src/main/java/me/binomial/contactsbuddy/DisplayContactsActivity.java
+++ app/src/main/java/me/binomial/contactsbuddy/DisplayContactsActivity.java
@@ -7,6 +7,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import me.binomial.contacts.ContactInfo;
+import me.binomial.contacts.ContactsFetcher;
 
 public class DisplayContactsActivity extends AppCompatActivity {
 
@@ -37,7 +43,10 @@ public class DisplayContactsActivity extends AppCompatActivity {
 
     private void displayContactsList()
     {
+        ArrayList<ContactInfo> contactsList = new ContactsFetcher(this).fetchAll();
 
+        // TODO: display in recyclerview
+        Toast.makeText(this, "Count of contacts: " + contactsList.size(), Toast.LENGTH_SHORT).show();
     }
 
     /* callback: When permission request is either accepted or rejected */
@@ -65,9 +74,6 @@ public class DisplayContactsActivity extends AppCompatActivity {
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 }
